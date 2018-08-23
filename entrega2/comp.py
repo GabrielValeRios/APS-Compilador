32e025628e8bc0740318564ddbdddaa31f7c3172
GabrielValeRios <contato_rios@hotmail.com>
2018-08-23 20:25:04 -0300

    tratamento de comentarios, espaços e *;/

--- entrega2/comp.py
+++ entrega2/comp.py
@@ -11,31 +11,31 @@ class Tokenizador():
 
 	def selecionarProximo(self):
 		listaTokens = []
-		flagDigit = True
+		firstNumber = True
 		resultado = ''
 		self.atual = self.origem[self.posicao]
 		while self.posicao <= (len(self.origem)) - 1: 
 			if self.atual.isdigit():
-				while flagDigit:
+				while firstNumber:
 					resultado += self.atual
 					if ((self.posicao == len(self.origem) - 1) or (self.origem[self.posicao + 1] == '+') or 
 						(self.origem[self.posicao + 1] == '-')):
 							self.posicao += 1
 							listaTokens.append(Token('INT', int(resultado)))
 							resultado = ''
-							flagDigit = False
+							firstNumber = False
 					else:
 						self.posicao += 1
 						self.atual = self.origem[self.posicao]
 
 			elif self.origem[self.posicao] == '+':
 				self.posicao += 1
-				flagDigit = True
+				firstNumber = True
 				listaTokens.append(Token('PLUS', '+'))
 
 			elif self.origem[self.posicao] == '-':
 				self.posicao += 1
-				flagDigit = True
+				firstNumber = True
 				listaTokens.append(Token('MINUS', '-'))
 
 			if self.posicao < len(self.origem):
@@ -54,25 +54,38 @@ class Analisador():
 
 	def analisarExpressao(self):
 		op = self.tokens.selecionarProximo()
-		flagDigit = True
+
+		firstNumber = True
+		firstTok = True
 		somar = False
 		subtrair = False
+
 		num = [0,0]
 		ans = None
 
 		for t in op:
-			if (t.tipo == 'INT') and (flagDigit == True):
+			if firstTok:
+				if t.tipo != 'INT':
+					raise ValueError('Expected INT, got {}'.format(t.tipo))
+				else:
+					firstTok = False
+			
+			if (t.tipo == 'INT') and (firstNumber == True):
 				num[0] += t.valor
 
 			elif t.tipo == 'PLUS':
+				if somar == True or subtrair == True :
+					raise ValueError('Expected INT after math signal, got {}'.format(t.tipo))
 				somar = True
-				flagDigit = False
+				firstNumber = False
 
 			elif t.tipo == 'MINUS':
+				if subtrair == True or somar == True :
+					raise ValueError('Expected INT after math signal, got {}'.format(t.tipo))
 				subtrair = True
-				flagDigit = False
+				firstNumber = False
 
-			elif (t.tipo == 'INT') and (flagDigit == False):
+			elif (t.tipo == 'INT') and (firstNumber == False):
 				num[1] += t.valor
 
 			if num[0] != 0 and num[1] != 0:
@@ -86,6 +99,8 @@ class Analisador():
 				num[1] = 0
 
 			if t.valor == "EOF":
+				if somar == True or subtrair == True:
+					raise ValueError('Find PLUS OR MINUS type at the end of expression. Not allowed')
 				ans = num[0]
 
 		return ans
---
+++ entrega3/comp.py
@@ -0,0 +1,175 @@
+class Token():
+	def __init__(self, tipo, valor):
+		self.tipo = tipo
+		self.valor = valor
+
+class Tokenizador():
+	def __init__(self,origem):
+		self.origem = origem
+		self.posicao = 0
+		self.atual = None
+		self.Tkatual = None
+
+	def selecionarProximo(self):
+		t = None
+		self.firstNumber = True
+		resultado = ''
+		if self.posicao <= (len(self.origem)) - 1: 
+			self.atual = self.origem[self.posicao]
+			if self.atual == ' ':
+				space = True
+				while space:
+					self.posicao+=1
+					if self.origem[self.posicao] != ' ':
+						space = False
+				self.atual = self.origem[self.posicao]
+			if self.atual == '/':
+				if self.origem[self.posicao + 1] =='*':
+					comentario = True
+					while comentario:
+						self.posicao+=1
+						if self.posicao+1 < len(self.origem):
+							self.atual = self.origem[self.posicao]
+							if self.atual == '*' and self.origem[self.posicao + 1] == '/':
+								self.posicao+=2
+								if self.posicao < len(self.origem):
+									self.atual = self.origem[self.posicao]
+									comentario = False
+								else:
+									t = Token('','EOF')
+									self.atual = ''
+									self.Tkatual = Token('','EOF')
+									comentario = False
+						else:
+							raise ValueError("Comentary not terminated.Expecting */")
+			if self.atual.isdigit():
+				while self.firstNumber:
+					resultado += self.atual
+					if ((self.posicao == len(self.origem) - 1) or (self.origem[self.posicao + 1] == '+') or 
+						(self.origem[self.posicao + 1] == '-') or (self.origem[self.posicao + 1] == '*') or
+						(self.origem[self.posicao + 1] == '/') or (self.origem[self.posicao + 1] == ' ')):
+							self.posicao += 1
+							t = Token('INT', int(resultado))
+							self.Tkatual = Token('INT', int(resultado))
+							resultado = ''
+							self.firstNumber = False
+					else:
+						self.posicao += 1
+						self.atual = self.origem[self.posicao]
+
+			elif self.atual == '+':
+				self.posicao += 1
+				self.firstNumber = True
+				t = Token('PLUS', '+')
+				self.Tkatual = Token('PLUS', '+')
+
+			elif self.atual == '-':
+				self.posicao += 1
+				self.firstNumber = True
+				t = Token('MINUS', '-')
+				self.Tkatual = Token('MINUS', '-')
+
+			elif self.atual == '*':
+				self.posicao += 1
+				self.firstNumber = True
+				t = Token('MULT', '*')
+				self.Tkatual = Token('MULT', '*')
+
+			elif self.atual == '/':
+				self.posicao += 1
+				print(self.origem[self.posicao])
+				self.firstNumber = True
+				t = Token('DIV', '/')
+				self.Tkatual = Token('DIV', '/')
+
+			elif self.atual == ' ':
+				self.posicao += 1
+				self.firstNumber = True
+
+			if self.posicao < len(self.origem):
+				self.atual = self.origem[self.posicao]
+
+		else:
+			t = Token('','EOF')
+			self.Tkatual = Token('','EOF')
+
+		return t
+
+	def getTokenAtual(self):
+		return self.Tkatual
+
+class Analisador():
+
+	tokens = None
+
+	def __init__(self, origem):
+		self.origem = origem
+		self.tokens = Tokenizador(origem)
+		self.isNotEOF = True
+
+	def term(self):
+		termLoop = True
+		resposta = None
+		while termLoop:
+			t = self.tokens.getTokenAtual()
+			if (t.tipo == 'INT'):
+				if resposta == None:
+					retorno = t.valor
+				else:
+					retorno = resposta
+				nextT = self.tokens.selecionarProximo()
+				if nextT.tipo == 'MULT':
+					nextT = self.tokens.selecionarProximo()
+					if nextT.tipo == 'INT':
+						retorno *= nextT.valor
+						resposta = retorno
+					else:
+						raise ValueError("Expected INT, got {} instead".format(nextT.valor))
+				elif nextT.tipo == 'DIV':
+					nextT = self.tokens.selecionarProximo()
+					if nextT.tipo == 'INT':
+						retorno //= nextT.valor
+						resposta = retorno
+					else:
+						raise ValueError("Expected INT, got {} instead".format(nextT.valor))
+				elif nextT.tipo == 'INT':
+					raise ValueError("Expected OPERATION, got {} instead".format(nextT.valor))
+				else:
+					termLoop = False
+			else:
+				raise ValueError("Expected INT, got {} instead".format(t.valor))
+
+		return retorno
+
+	def analisarExpressao(self):
+
+		self.tokens.selecionarProximo()
+		retorno = self.term()
+		while self.isNotEOF:
+			if self.tokens.getTokenAtual().tipo == 'PLUS':
+				nextT = self.tokens.selecionarProximo()
+				if nextT.tipo == 'INT':
+					retorno += self.term()
+				else:
+					raise ValueError("Expected INT, got {} instead".format(nextT.valor))
+			elif self.tokens.getTokenAtual().tipo == 'MINUS':
+				nextT = self.tokens.selecionarProximo()
+				if nextT.tipo == 'INT':
+					retorno -= self.term()
+				else:
+					raise ValueError("Expected INT, got {} instead".format(nextT.valor))
+			elif self.tokens.getTokenAtual().valor == 'EOF':
+				self.isNotEOF = False
+			else:
+				self.term()
+
+		return retorno
+
+if __name__ == "__main__":
+
+	with open("testes.txt") as f:
+	    content = f.readlines()
+
+	contentClean = [x.strip().replace(" ","") for x in content]
+	for expression in content: 
+		print(Analisador(expression).analisarExpressao())
