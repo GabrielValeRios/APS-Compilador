c7dbbced528f09d5e653fed3fb38bdc779f21c34
GabrielValeRios <contato_rios@hotmail.com>
2018-11-16 17:17:29 -0200

    func working

--- entrega9/comp.py
+++ entrega9/comp.py
@@ -44,6 +44,11 @@ class Tokenizador():
 						raise ValueError("Comentary not terminated.Expecting */")
 		return t
 
+	def backwards(self,typo,value):
+		self.position -= 1
+		self.current = self.origin[self.position]
+		self.Tkcurrent = Token(typo, value)
+
 	def selectNext(self):
 		t = None
 		self.firstNumber = True
@@ -120,9 +125,9 @@ class Tokenizador():
 					t = Token('VOID', resultIdent)
 					self.Tkcurrent = t
 					resultIdent = ''
-				elif resultIdent == "main":
+				elif resultIdent == "return":
 					self.firstNumber = True
-					t = Token('MAIN', resultIdent)
+					t = Token('RETURN', resultIdent)
 					self.Tkcurrent = t
 					resultIdent = ''
 
@@ -369,7 +374,7 @@ class Analyser():
 		if t.typo == "VOID" or t.typo == "INT" or t.typo == "CHAR":	
 			return True
 		return False
-
+	"""
 	def mainEvaluate(self):
 		t = self.tokens.getTokencurrent()
 
@@ -393,11 +398,62 @@ class Analyser():
 				raise ValueError("main needed")
 		else:
 			raise ValueError("typo missing")
-
-	
-	def statments(self):
+	"""
+	def funcStatments(self):
 		t = self.tokens.getTokencurrent()
+		childrens = []
+		while t.typo != None:
+			if self.typeRecognize():
+				funcDecList = []
+				t = self.tokens.getTokencurrent()
+				typo_temp = t.typo
+				funcDecList.append(typo_temp)
+				self.tokens.selectNext()
+				t = self.tokens.getTokencurrent()
+				typo = t.typo
+				value = t.value
+				self.tokens.selectNext()
+				t = self.tokens.getTokencurrent()
+				if t.typo == "PAREN": 	
+					self.tokens.backwards(typo,value)
+					t = self.tokens.getTokencurrent()
+					funcName = t.value
+					self.tokens.selectNext()
+					self.tokens.selectNext()
+					while True:
+						if self.typeRecognize():
+							t = self.tokens.getTokencurrent()
+							typo_temp = t.typo
+							self.tokens.selectNext()
+							t = self.tokens.getTokencurrent()
+							value = t.value
+							funcDecList.append(VarDecNode(typo_temp,[value]))
+							self.tokens.selectNext()
+							t = self.tokens.getTokencurrent()
+							if t.typo == "COMMA":
+								self.tokens.selectNext()
+								continue
+							else:
+								break
+						else:
+							break
+					self.tokens.selectNext()
+					commandsStuff = self.statments()
+					funcDecList.append(commandsStuff)
+					childrens.append(funcDec(funcName,funcDecList))
+					self.tokens.selectNext()
+					t = self.tokens.getTokencurrent()
+			else:
+				t.typo = None
 
+		mainCall = funcCall('main',[])
+		childrens.append(mainCall)
+		master = masterNode(childrens)
+		return master
+
+	def statments(self):
+
+		t = self.tokens.getTokencurrent()
 		if t.typo == "BRACKETS":
 			if t.value == "{":
 				statments = []
@@ -418,19 +474,28 @@ class Analyser():
 		t = self.tokens.getTokencurrent()
 		if t.typo == "IDENTIFIER":
 			return self.atribution()
+
 		elif self.typeRecognize():
 			typo_temp = t.typo
 			self.tokens.selectNext()
 			t = self.tokens.getTokencurrent()
-			varDecList.append(t.value)
-			while t.typo != "SEMI-COLON":
-				self.tokens.selectNext()
+			typo = t.typo
+			value = t.value
+			self.tokens.selectNext()
+			t = self.tokens.getTokencurrent()
+			if t.typo != "PAREN":
 				t = self.tokens.getTokencurrent()
-				if t.typo == "COMMA":
+				self.tokens.backwards(typo,value)
+				t = self.tokens.getTokencurrent()
+				varDecList.append(t.value)
+				while t.typo != "SEMI-COLON":
 					self.tokens.selectNext()
 					t = self.tokens.getTokencurrent()
-					varDecList.append(t.value)
-			return VarDecNode(typo_temp,varDecList)
+					if t.typo == "COMMA":
+						self.tokens.selectNext()
+						t = self.tokens.getTokencurrent()
+						varDecList.append(t.value)
+				return VarDecNode(typo_temp,varDecList)
 		elif t.typo == "PRINTF":
 			self.tokens.selectNext()
 			t = self.tokens.getTokencurrent()
@@ -493,6 +558,12 @@ class Analyser():
 
 			return WhileNode([condition,trueState])
 
+		elif t.typo == "RETURN":
+			self.tokens.selectNext()
+			t = self.tokens.getTokencurrent()
+			r = returnNode([self.parseExpression()])
+			return r
+
 		elif t.typo == "BRACKETS":
 			if t.value == "{":
 				ans = self.statments()
@@ -524,10 +595,37 @@ class Analyser():
 							return BinOp('=',[var,ans])						
 						else:
 							raise ValueError("erro parentes scanf")
+			elif t.typo == "IDENTIFIER":
+				funcName = t.value
+				typo = t.typo
+				value = t.value
+				self.tokens.selectNext()
+				t = self.tokens.getTokencurrent()
+
+				if t.typo == "PAREN":
+					if t.value == "(":
+						args  = []
+						self.tokens.selectNext()
+						while t.value != ")":	
+							ans = self.parseExpression()
+							args.append(ans)
+							t = self.tokens.getTokencurrent()
+							if t.typo == "COMMA":
+								self.tokens.selectNext()
+								t = self.tokens.getTokencurrent()
+						
+						self.tokens.selectNext()
+						t = self.tokens.getTokencurrent()
+						if t.typo == "SEMI-COLON":
+							return BinOp('=',[var,funcCall(funcName,args)])
+				else:
+					self.tokens.backwards(typo,value)
+					t = self.tokens.getTokencurrent()
+					return BinOp('=',[var,self.parseExpression()])
 			else:
 				return BinOp('=',[var,self.parseExpression()])
 		else:
-			raise("erro-Atribuiton")
+			raise ValueError("erro-Atribuiton")
 
 class Node():
 	def __init__(self):
@@ -535,6 +633,61 @@ class Node():
 		self.children = []
 	def Evaluate():
 		pass
+
+class masterNode(Node):
+	def __init__(self,children):
+		self.children = children
+
+	def Evaluate(self):
+		symbolTable = SymbolTable()
+		for i in self.children:
+			i.Evaluate(symbolTable)
+
+class returnNode(Node):
+	def __init__(self, children):
+		self.children = children
+
+	def Evaluate(self,symbolTable):
+		ans = self.children[0].Evaluate(symbolTable)
+		return ans
+
+class funcDec(Node):
+	def __init__(self,value,children):
+		self.value = value
+		self.children = children
+		
+	def Evaluate(self,symbolTable):
+		symbolTable.createValue('func',self.value)
+		symbolTable.setValue(self.value,self)
+
+class funcCall(Node):
+	def __init__(self,value,children):
+		self.value = value
+		self.children = children
+		self.NewSymbolTable = SymbolTable() #Passar a symboltable atual como ancestor
+
+	def Evaluate(self,symbolTable):
+		self.NewSymbolTable.ancestor = symbolTable
+
+		func = symbolTable.getValue(self.value)
+		argsName = []
+		for i in range(1, len(func.children)-1):
+			ref = func.children[i].children[0]
+			argsName.append(ref) #precisa guardar o nome da variável aqui
+			func.children[i].Evaluate(self.NewSymbolTable) #Declarou os argumentos na nova ST
+
+		if (len(self.children) != 0):
+			for i, j in enumerate(self.children):
+				self.NewSymbolTable.setValue(argsName[i], j.Evaluate(symbolTable)) #passar o valor dos filhos para a nova ST na ordem correta
+
+        #Evaluate do ultimo filho (comandos)
+		for e in func.children[len(func.children)-1].children:
+			tp = type(e) #aquiii
+			typo = "<class '__main__.returnNode'>"
+			if str(tp)  == typo:
+				return e.Evaluate(self.NewSymbolTable)
+			else:
+				e.Evaluate(self.NewSymbolTable)
 
 class StatmentsNode(Node):
 	def __init__(self, children):
@@ -644,31 +797,38 @@ class VarDecNode(Node):
 
 	def Evaluate(self,symbolTable):
 		for i in self.children:
-			symbolTable.setValue(i,self.value)
+			symbolTable.createValue(self.value,i)
 
 class SymbolTable():
 	def __init__(self):
 		self.symbolTable = {}
+		self.ancestor = None 
 
 	def getValue(self, key):
 		if key in self.symbolTable:
 			return self.symbolTable.get(key)[1]
 		else:
-			raise ValueError("key '{}' do not exists".format(key))
+			if self.ancestor:
+				return self.ancestor.getValue(key)
+			else:
+				raise ValueError("Senpai")
 
 	def setValue(self, key, value):
 		if key in self.symbolTable:
 			keyType = self.symbolTable.get(key)[0].lower()
 			valueType = value
-			if "<class '{}'>".format(keyType) == str(type(valueType)):
-				self.symbolTable.get(key).append(value)
+			if "<class '{}'>".format(keyType) == str(type(valueType)) or keyType == 'func':
+				self.symbolTable[key][1] = value
 			else:
 				raise ValueError("wrong type for variable")
 		else:
-			self.symbolTable['{}'.format(key)] = [value]
+			raise ValueError("setValue error")
+
+	def createValue(self,typo,key):
+		self.symbolTable['{}'.format(key)] = [typo,None]
+
 
 def main():
-	symbolTable = SymbolTable()
 
 	with open("testes.c") as f:
 		content = f.readlines()
@@ -678,8 +838,8 @@ def main():
 	for line in content:
 		code+=line
 
-	root = Analyser(code).mainEvaluate()
-	root.Evaluate(symbolTable)
+	root = Analyser(code).funcStatments()
+	root.Evaluate()
 
 if __name__ == "__main__":
 	main()
--- entrega9/testes.c
+++ entrega9/testes.c
@@ -1,26 +1,31 @@
+int func1(int x){
+    int t;
+    t = x;
+    return t;
+}
+
 void main()
 {
-    /*
-    comentarios
-    comentarios
-    comentarios
-    */
-    int x,y;
-    x = 3;
+    
+    int x,y;    
+    x = 3;   
     y = 2;
-    if (x > y || x == 2) then{ /* só aceita */
+
+    
+    if (x > y || x == 2) then{ 
         printf(x);
     }
     else {
         printf(y);
     };
-
+    
     printf(x + y);
     printf(x - -y);
-    printf(    /* bla */ 1 /* bla */);
     printf(  3+4/2 + y);
+    
     int z;
     z = x + y;
+
     printf(x + y + z);
     int a;
     a = scanf();
@@ -28,11 +33,17 @@ void main()
 
     printf(x);
     printf(y);
-
+    
     int p;
     p = 10;
+    /*
     while(p>0) then{
         p = p-1;
         printf(p);
     };
+    */
+    int v;
+    v = func1(p);
+    printf(v+1);
+    
 }
